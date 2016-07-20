9ab02131dc5e642bed4594d58927429ab9392d97
A. Hahn <a.hahn@gsi.de>
2016-07-20 11:32:07 +0200

    nightly: modified git user.name

--- scripts/nightly/git_init.sh
+++ scripts/nightly/git_init.sh
@@ -2,5 +2,5 @@
 
 git submodule init
 git submodule update --recursive
-git config --global user.name "Jenkins"
+git config --global user.name "Jenkins Nightly Build"
 git config --global user.email "csco-tg@gsi.de"
