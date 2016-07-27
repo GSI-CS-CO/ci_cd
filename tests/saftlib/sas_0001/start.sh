e4567debae021b814f691cdcd91616d5e4a48358
A. Hahn <a.hahn@gsi.de>
2016-07-27 17:30:20 +0200

    sas_0001: increased test duration

--- tests/saftlib/sas_0001/start.sh
+++ tests/saftlib/sas_0001/start.sh
@@ -2,6 +2,8 @@
 
 device=$(head /tmp/saftlib_dev -n 1)
 
-timeout 30m ./test.py $device 25 10
-timeout 30m ./test.py $device 250 10
-timeout 30m ./test.py $device 2500 10
+timeout 30m ./test.py $device 1 10000
+timeout 30m ./test.py $device 10 1000
+timeout 30m ./test.py $device 100 100
+timeout 30m ./test.py $device 1000 10
+timeout 30m ./test.py $device 10000 1
