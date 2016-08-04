1a48f5f0b8257a1d78eead85eaf69cf89f695637
Jenkins <csco-tg@gsi.de>
2016-08-04 10:49:24 +0200

    Script added to the repository are 1. flash devices in the testing facility 2. scu reset script 3. Script to program and flash devices in CI facility

--- scripts/nightly/filecopy_websrvr.sh
+++ scripts/nightly/filecopy_websrvr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export WEB_SRVR_PATH=/var/www/html/releases/nightly/gateware
+export WEB_SRVR_PATH=/var/www/html/releases/nightly
 
 PWD=$(pwd)
 
@@ -9,82 +9,129 @@ export pex_path=/home/timing/jenkins_jobs/nightly_build_pexarria5
 export vet_path=/home/timing/jenkins_jobs/nightly_build_vetar2a
 export scu3_path=/home/timing/jenkins_jobs/nightly_build_scu3
 export scu2_path=/home/timing/jenkins_jobs/nightly_build_scu2
-
+export dm_path=/home/timing/jenkins_jobs/nightly_build_datamaster
+export wrpc_path=/home/timing/jenkins_jobs/nightly_build_wrpc-sw
+export eb_path=/home/timing/jenkins_jobs/nightly_build_etherbone
+export saftlib_path=/home/timing/jenkins_jobs/nightly_build_saftlib
 
 case $PWD in
 
 	$exp_path)
 				
 		make exploder5-clean
+                export PATH=$PATH:$exp_path/toolchain/bin
 		make exploder5
-		
-		export PATH=$PATH:$exp_path/toolchain/bin
-		
+				
 		cd syn/gsi_exploder5/exploder5_csco_tr
-		cp exploder5_csco_tr.jic exploder5_csco_tr.rpd exploder5_csco_tr.sof $WEB_SRVR_PATH
+		cp exploder5_csco_tr.jic exploder5_csco_tr.rpd exploder5_csco_tr.sof $WEB_SRVR_PATH/gateware
 
-		cd $WEB_SRVR_PATH
+		cd $WEB_SRVR_PATH/gateware
 		md5sum *.jic *.rpd *.sof | tee MD5SUMS
 	;;
 
 	$pex_path)
 
 		make pexarria5-clean
+                export PATH=$PATH:$pex_path/toolchain/bin
 		make pexarria5
 
-		export PATH=$PATH:$pex_path/toolchain/bin
-
 		cd syn/gsi_pexarria5/control
-		cp  pci_control.jic pci_control.sof pci_control.rpd $WEB_SRVR_PATH
+		cp  pci_control.jic pci_control.sof pci_control.rpd $WEB_SRVR_PATH/gateware
 
-		cd $WEB_SRVR_PATH
+		cd $WEB_SRVR_PATH/gateware
 		md5sum *.jic *.rpd *.sof | tee MD5SUMS
 	;;
 
 	$vet_path)
 
 		make vetar2a-clean
+                export PATH=$PATH:$vet_path/toolchain/bin
 		make vetar2a
 
-		export PATH=$PATH:$vet_path/toolchain/bin
-
 		cd syn/gsi_vetar2a/wr_core_demo
-		cp vetar2a.jic vetar2a.rpd vetar2a.sof $WEB_SRVR_PATH
+		cp vetar2a.jic vetar2a.rpd vetar2a.sof $WEB_SRVR_PATH/gateware
 
-		cd $WEB_SRVR_PATH
+		cd $WEB_SRVR_PATH/gateware
 		md5sum *.jic *.rpd *.sof | tee MD5SUMS
         ;;
 
 	$scu3_path)
 
 		make scu3-clean
+                export PATH=$PATH:$scu3_path/toolchain/bin
 		make scu3
 
-		export PATH=$PATH:$scu3_path/toolchain/bin
-
 		cd syn/gsi_scu/control3
-		cp scu_control.rpd $WEB_SRVR_PATH/scu_control3.rpd
-		cp scu_control.sof $WEB_SRVR_PATH/scu_control3.sof
-		cp scu_control.jic $WEB_SRVR_PATH/scu_control3.jic
+		cp scu_control.rpd $WEB_SRVR_PATH/gateware/scu_control3.rpd
+		cp scu_control.sof $WEB_SRVR_PATH/gateware/scu_control3.sof
+		cp scu_control.jic $WEB_SRVR_PATH/gateware/scu_control3.jic
 
-		cd $WEB_SRVR_PATH
+		cd $WEB_SRVR_PATH/gateware
 		md5sum *.jic *.rpd *.sof | tee MD5SUMS
 	;;
 
 	$scu2_path)
 
                 make scu2-clean
+		export PATH=$PATH:$scu2_path/toolchain/bin
                 make scu2
 
-		export PATH=$PATH:$scu2_path/toolchain/bin
-
                 cd syn/gsi_scu/control2
-                cp scu_control.rpd $WEB_SRVR_PATH/scu_control2.rpd
-                cp scu_control.sof $WEB_SRVR_PATH/scu_control2.sof
-                cp scu_control.jic $WEB_SRVR_PATH/scu_control2.jic
+                cp scu_control.rpd $WEB_SRVR_PATH/gateware/scu_control2.rpd
+                cp scu_control.sof $WEB_SRVR_PATH/gateware/scu_control2.sof
+                cp scu_control.jic $WEB_SRVR_PATH/gateware/scu_control2.jic
 
-                cd $WEB_SRVR_PATH
+                cd $WEB_SRVR_PATH/gateware
                 md5sum *.jic *.rpd *.sof | tee MD5SUMS
         ;;
+
+        $dm_path)
+
+                make firmware
+		cd syn/gsi_pexarria5/ftm
+                export PATH=$PATH:$dm_path/toolchain/bin
+		make
+		
+		cp ftm.jic ftm.rpd ftm.sof $WEB_SRVR_PATH/gateware
+                
+                cd $WEB_SRVR_PATH/gateware
+                md5sum *.jic *.rpd *.sof | tee MD5SUMS
+        ;;
+
+        $wrpc_path)
+
+                make 
+                cd ip_cores/wrpc-sw        
+                export PATH=$PATH:$wrpc_path/toolchain/bin
+                make
+		
+		cp wrc.bin wrc.elf wrc.mif $WEB_SRVR_PATH/wrpc-sw
+        ;;
+
+	$eb_path)
+
+                make etherbone
+                export PATH=$PATH:$eb_path/toolchain/bin
+                make etherbone-install STAGING=/tmp/etherbone
+                cd /tmp
+		
+		tar -cf - etherbone | xz -9 -c - > etherbone.tar.xz
+		cp etherbone.tar.xz $WEB_SRVR_PATH/etherbone
+		rm -rf etherbone etherbone.tar.xz
+        ;;
+
+        $saftlib_path)
+
+                make saftlib
+                export PATH=$PATH:$saftlib_path/toolchain/bin
+                make saftlib-install STAGING=/tmp/saftlib
+		cd /tmp
+
+                tar -cf - saftlib | xz -9 -c - > saftlib.tar.xz
+                cp saftlib.tar.xz $WEB_SRVR_PATH/saftlib
+                rm -rf saftlib saftlib.tar.xz
+        ;;
+
+
 esac
 
---
+++ scripts/nightly/flash.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+export WEB_SERVER=http://tsl002.acc.gsi.de/releases/nightly/gateware
+
+if [ $# == 0 ]; then
+        NIGHTLY=./nightly_files
+        mkdir $NIGHTLY
+	echo -e "\e[33mDefault directory $NIGHTLY created to save files"
+else
+        mkdir $1
+        NIGHTLY=$1
+	echo -e "\e[33mUser defined directory $NIGHTLY created to save files"
+fi
+
+wget $WEB_SERVER/device-list.txt -O $NIGHTLY/device-list.txt
+wget $WEB_SERVER/exploder5_csco_tr.rpd -O $NIGHTLY/exploder5_csco_tr.rpd
+wget $WEB_SERVER/pci_control.rpd -O $NIGHTLY/pci_control.rpd
+wget $WEB_SERVER/vetar2a.rpd -O $NIGHTLY/vetar2a.rpd
+wget $WEB_SERVER/scu_control3.rpd -O $NIGHTLY/scu_control3.rpd
+wget $WEB_SERVER/scu_control2.rpd -O $NIGHTLY/scu_control2.rpd
+wget $WEB_SERVER/ftm.rpd -O $NIGHTLY/ftm.rpd
+
+list=$NIGHTLY/device-list.txt
+temp=$NIGHTLY/tmp.txt
+
+echo -e "\e[96mEnter the keyword of device name to flash"
+echo -e "\e[33mAccepted keyword is exp,pex,vet,scu2,scu3,dm,all"
+
+read input 
+
+if [ "$input" == "exp" ] || [ "$input" == "all" ]; then
+	##Search for keyword exploder in the device list and store the value to a text file
+	grep -ie "exploder" $list > $temp
+
+	##Create an array with the values from the text file created in previous step
+	while IFS=$'\t' read -r -a nightlyArray
+	do
+		for i in {nightlyArray[2]}
+		do
+			sudo eb-flash udp/${nightlyArray[2]} $NIGHTLY/exploder5_csco_tr.rpd
+			echo -e "\e[34m${nightlyArray[0]} with IP ${nightlyArray[2]} flashed with latest exploder5_csco_tr.rpd gateware"
+			echo
+		done
+	done < $temp
+else
+        echo -e "\e[33mKeyword used is $input, not flashing exploder" 
+fi
+	
+if [ "$input" == "pex" ] || [ "$input" == "all" ]; then
+	##Search for keyword pexarria in the device list and store the value to a text file
+	grep -ie "pexarria" $list > $temp
+
+	##Create an array with the values from the text file created in previous step
+	while IFS=$'\t' read -r -a nightlyArray
+	do
+        	for i in {nightlyArray[2]}
+	        do
+	       	        sudo eb-flash udp/${nightlyArray[2]} $NIGHTLY/pci_control.rpd
+                	echo -e "\e[34m${nightlyArray[0]} with IP ${nightlyArray[2]} flashed with latest pci_control.rpd gateware"
+	                echo
+        	done
+	done < $temp
+else
+        echo -e "\e[33mKeyword used is $input, not flashing pexarria" 
+fi
+
+if [ "$input" == "vet" ] || [ "$input" == "all" ]; then
+	##Search for keyword pexarria in the device list and store the value to a text file
+	grep -ie "vetar" $list > $temp
+
+	##Create an array with the values from the text file created in previous step
+	while IFS=$'\t' read -r -a nightlyArray
+	do
+        	for i in {nightlyArray[2]}
+	        do
+	    	        sudo eb-flash udp/${nightlyArray[2]} $NIGHTLY/vetar2a.rpd
+                	echo -e "\e[34m${nightlyArray[0]} with IP ${nightlyArray[2]} flashed with latest vetar2a.rpd gateware"
+	                echo
+        	done
+	done < $temp
+else
+        echo -e "\e[33mKeyword used is $input, not flashing vetar" 
+fi
+
+if [ "$input" == "scu3" ] || [ "$input" == "all" ]; then
+	##Search for keyword pexarria in the device list and store the value to a text file
+	grep -ie "scu3" $list > $temp
+
+	##Create an array with the values from the text file created in previous step
+	while IFS=$'\t' read -r -a nightlyArray
+	do
+        	for i in {nightlyArray[2]}
+	        do
+ 	  	        sudo eb-flash udp/${nightlyArray[2]} $NIGHTLY/scu_control3.rpd
+                	echo -e "\e[34m${nightlyArray[0]} with IP ${nightlyArray[2]} flashed with latest scu_control3.rpd gateware"
+	                echo
+	     	. ./scu_reset.sh ${nightlyArray[0]} ${nightlyArray[2]}
+		done
+	done < $temp
+else
+        echo -e "\e[33mKeyword used is $input, not flashing scu3" 
+fi
+
+if [ "$input" == "scu2" ] || [ "$input" == "all" ]; then
+	##Search for keyword pexarria in the device list and store the value to a text file
+	grep -ie "scu2" $list > $temp
+
+	##Create an array with the values from the text file created in previous step
+	while IFS=$'\t' read -r -a nightlyArray
+	do
+        	for i in {nightlyArray[2]}
+	        do
+	       	        sudo eb-flash udp/${nightlyArray[2]} $NIGHTLY/scu_control2.rpd
+                	echo -e "\e[34m${nightlyArray[0]} with IP ${nightlyArray[2]} flashed with latest scu_control2.rpd gateware"
+	                echo
+		. ./scu_reset.sh ${nightlyArray[0]} ${nightlyArray[2]}
+        	done
+	done < $temp
+else
+        echo -e "\e[33mKeyword used is $input, not flashing scu2" 
+fi
+
+if [ "$input" == "dm" ] || [ "$input" == "all" ]; then
+	##Search for keyword pexarria in the device list and store the value to a text file
+	grep -ie "datamaster" $list > $temp
+
+	##Create an array with the values from the text file created in previous step
+	while IFS=$'\t' read -r -a nightlyArray
+	do
+        	for i in {nightlyArray[2]}
+	        do
+	       	        sudo eb-flash udp/${nightlyArray[2]} $NIGHTLY/ftm.rpd
+	                echo -e "\e[34m${nightlyArray[0]} with IP ${nightlyArray[2]} flashed with latest ftm.rpd gateware"
+        	        echo
+	        done
+
+	done < $temp
+else
+	echo -e "\e[33mKeyword used is $input, not flashing datamaster" 
+fi
+
+if [ "$input" != "exp" ] && [ "$input" != "pex" ] && [ "$input" != "vet" ] && [ "$input" != "scu3" ] && [ "$input" != "scu2" ] && [ "$input" != "dm" ] && [ "$input" != "all" ]; then
+	echo -e "\e[31mIncorrect keyword. Please try again"
+fi
+
+rm $temp
+rm -rf $NIGHTLY
