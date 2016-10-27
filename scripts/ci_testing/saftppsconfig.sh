13d1f7c3fbad80315103b93556f6454f16a30394
A.Suresh <a.suresh@gsi.de>
2016-10-27 17:33:16 +0200

    saftppsconfig: Modified script to pass arguments to select the reference device for time difference calculation. Gives error if no argument is passed.

--- scripts/ci_testing/saftppsconfig.sh
+++ scripts/ci_testing/saftppsconfig.sh
@@ -1,31 +1,60 @@
 #!/bin/bash
-
+set -e
 #This is the configuration file used by PPS generation scripts
 
 #Temporary log file
-exp_IO1_log1=./exp_IO1_log1.log
+dev1_log1=./dev1_log1.log
 
 #Log file created after PPS signals are read on the exploder from other devices
-exp_IO1_log=./exp_IO1_log.log
+dev1_log=./dev1_log.log
 
 #Log file created during PPS signal generation on Pexarria
-pex_IO1_log1=./pex_IO1_log1.log
+dev2_log1=./dev2_log1.log
 
 #Intermediate files that will be deleted
-expIOtemp1=./exp_IO1_log.log1
-expIOtemp2=./exp_IO1_log.log2
-
-#Network switch PPS signal port connected to IO1 of Exploder
-#Difference in time is calculated with this signal as reference.
-ref1="IO1"
-
-#Available ports in Exploder5a_19t
-ref=( IO1 IO2 IO3 IO4 IO5 IO6 IO7 IO8 )
-
-#Devices that are connected to the IO ports of Exploder5a_19t
-#in the order from port 1 to 8
-dev=( Nw_switch pexaria5_40t scuxl0097t no_device no_device no_device no_device no_device )
+dev1temp1=./dev1temp1.log1
+dev1temp2=./dev1temp2.log2
 
 #Email address to send error information
 #Add addresses by giving a space between each email address
 mail="a.suresh@gsi.de"
+
+#Maximum acceptable difference in time
+maxdiff=200
+
+#Waiting time for the pps signal generation
+sleep_time=60
+
+#Number of lines from the output of pps signal generation to be considered for time difference calculation
+line_num=403
+
+#Case statement to check if any configuration is passed as an argument
+if [ "$1" != "" ]; then
+	case "$1" in
+#Configuration when exploder is considered as the reference device in the argument passed
+		exp_config)
+#Reference port for time difference calculation
+			ref1=IO1
+#Available ports for exploder as the reference device
+			ref=( IO1 IO2 IO3 IO4 IO5 IO6 IO7 IO8 )
+#Devices connected to exploder in the same order as the ports
+			dev=( NW_SW PEX SCU3 No_Dev No_Dev No_Dev No_Dev No_Dev )
+		;;
+
+#Configuration when pexarria is considered as the reference device in the argument passed
+		pex_config)
+#Reference port for time difference calculation
+			ref1=IO1
+#Available ports for pexarria as the reference device
+                        ref=( IO1 IO2 IO3 )
+#Devices connected to pexarria in the same order as the ports
+                        dev=( NW_SW EXP SCU3 )
+		;;
+
+		*) echo -e "\e[31mInternal error! Check argument" ; exit 1 ;;
+	esac
+else
+	echo -e "\e[31mNo configuration was passed as argument. Pass an argument before running the script"
+	echo -e "\e[31mAccepted arguments are exp_config pex_config"
+	exit 1
+fi
--- scripts/ci_testing/saftppstest.sh
+++ scripts/ci_testing/saftppstest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 #This script is used to generate PPS signal on Pexarria and SCU3.
 #The signals from Network switch, Pexarria and SCU3 are read on exploder 
 #to calculate the time difference between network switch and the devices.
@@ -18,17 +18,17 @@ saft-io-ctl exp -n IO2 -o 0 -t 1
 saft-io-ctl exp -n IO3 -o 0 -t 1
 
 #Generating PPS signal on pexarria and writing the data to log file
-saft-pps-gen pex -s > $pex_IO1_log1 &
+saft-pps-gen pex -s > $dev2_log1 &
 
 #Reading the PPS data on all configured ports of exploder and writing the data to temporary log file
-saft-io-ctl exp -s > $exp_IO1_log1 &
+saft-io-ctl exp -s > $dev1_log1 &
 
 #Wiping all the existing configuration on.
 #Generating PPS signal on SCU3 and killing the process after a delay
 ssh root@scuxl0097.acc.gsi.de '
 saft-io-ctl baseboard -w
 saft-pps-gen baseboard -s &
-sleep 60
+sleep '$sleep_time'
 
 echo $!
 kill $!
@@ -49,17 +49,17 @@ kill $pid ;
 done
 
 #Counting the number of lines in temporary log file
-exp_IO1_count=$(wc -l < $exp_IO1_log1)
+count=$(wc -l < $dev1_log1)
 
 #If a minimum of 60 lines are available in the temporary log file, then data acquired
 #is sufficient to calculate the time difference. To reduce the size of the file, all 
 #the lines after 403 are deleted. This is an arbitrary number.
-if [ "$exp_IO1_count" -ge "60" ]; then
+if [ "$count" -ge "$sleep_time" ]; then
         echo "Count reached required limit";
-	sed '403,$d' $exp_IO1_log1 > $exp_IO1_log
+	sed ''$line_num',$d' $dev1_log1 > $dev1_log
 #Deleting the temporary file after storing the required data in exploder log file
-	rm $exp_IO1_log1
+	rm $dev1_log1
 fi
 
 #Deleting intermediate log file used for pexarria data storage 
-rm $pex_IO1_log1
+rm $dev2_log1
