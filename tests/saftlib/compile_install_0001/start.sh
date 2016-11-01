196e9d0e1fb813593a423aa9818f55e80ae7df14
A. Hahn <a.hahn@gsi.de>
2016-11-01 13:28:06 +0100

    saftlib: added second installation source

--- tests/saftlib/compile_install_0001/start.sh
+++ tests/saftlib/compile_install_0001/start.sh
@@ -1,24 +1,51 @@
 #!/bin/bash
 
+# Settings
+production_repo="https://github.com/GSI-CS-CO/bel_projects.git"
+production_branch="balloon"
+development_repo="https://github.com/GSI-CS-CO/saftlib.git"
+development_branch="master"
+
 # Create/change to workspace directory
 if [ -d workspace ]; then
   mkdir workspace
 fi
-
-# Remove old saftlib stuff
 cd workspace
-if [ -d saftlib ]; then
-  rm -rf saftlib
-fi
 
-# Get latest saftlib master
-git clone https://github.com/GSI-CS-CO/saftlib.git saftlib
-cd saftlib
-git clean -xfd .
-
-# Compile and install
-./autogen.sh
-./configure --enable-maintainer-mode --prefix=/usr --sysconfdir=/etc
-make clean
-make
-sudo make install
+# Select installation source
+case "$1" in
+  "production")
+    if [ -d bel_projects ]; then
+      rm -rf bel_projects
+    fi
+    git clone $production_repo bel_projects
+    cd bel_projects
+    git checkout $production_branch
+    git submodule init
+    git submodule update --recursive
+    make
+    make saftlib
+    sudo make saftlib-install
+    ;;
+  "development")
+    if [ -d saftlib ]; then
+      rm -rf saftlib
+    fi
+    git clone $development_repo saftlib
+    cd saftlib
+    git clean -xfd .
+    git checkout $development_branch
+    ./autogen.sh
+    ./configure --enable-maintainer-mode --prefix=/usr --sysconfdir=/etc
+    make clean
+    make
+    sudo make install
+    ;;
+  *)
+    echo "Please specify the installation source"
+    echo "Available sources are:"
+    echo "  - production (bel_projects repository (submodule) -> $production_branch)"
+    echo "  - development (saftlib repository -> $development_branch)"
+    exit 1
+    ;;
+esac
