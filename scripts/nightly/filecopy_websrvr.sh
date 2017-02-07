8c68165a459544db0986ddc82ea386bdc0af220b
A.Suresh <a.suresh@gsi.de>
2017-02-07 17:01:29 +0100

    filecopy_websrvr.sh: Added GSI_BUILD_TYPE global parameter to the script

--- scripts/nightly/filecopy_websrvr.sh
+++ scripts/nightly/filecopy_websrvr.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-export WEB_SRVR_PATH=/var/www/html/releases/nightly
+WEB_SRVR_PATH=/var/www/html/releases/nightly
 
 PWD=$(pwd)
 
-export exp_path=/home/timing/jenkins_jobs/nightly_build_exploder5
-export pex_path=/home/timing/jenkins_jobs/nightly_build_pexarria5
-export vet_path=/home/timing/jenkins_jobs/nightly_build_vetar2a
-export scu3_path=/home/timing/jenkins_jobs/nightly_build_scu3
-export scu2_path=/home/timing/jenkins_jobs/nightly_build_scu2
-export dm_path=/home/timing/jenkins_jobs/nightly_build_datamaster
-export wrpc_path=/home/timing/jenkins_jobs/nightly_build_wrpc-sw
-export eb_path=/home/timing/jenkins_jobs/nightly_build_etherbone
-export saftlib_path=/home/timing/jenkins_jobs/nightly_build_saftlib
+exp_path=/home/timing/jenkins_jobs/nightly_build_exploder5
+pex_path=/home/timing/jenkins_jobs/nightly_build_pexarria5
+vet_path=/home/timing/jenkins_jobs/nightly_build_vetar2a
+scu3_path=/home/timing/jenkins_jobs/nightly_build_scu3
+scu2_path=/home/timing/jenkins_jobs/nightly_build_scu2
+dm_path=/home/timing/jenkins_jobs/nightly_build_datamaster
+wrpc_path=/home/timing/jenkins_jobs/nightly_build_wrpc-sw
+eb_path=/home/timing/jenkins_jobs/nightly_build_etherbone
+saftlib_path=/home/timing/jenkins_jobs/nightly_build_saftlib
+log_makefile=/var/www/html/releases/log/nightly_makefile_log
 
 case $PWD in
 
@@ -20,7 +21,8 @@ case $PWD in
 				
 		make exploder5-clean
                 export PATH=$PATH:$exp_path/toolchain/bin
-		make exploder5
+		export GSI_BUILD_TYPE=Balloon_release
+		make exploder5 2>&1 > $log_makefile/nightly_build_exploder5.$(date +%Y-%m-%d).log
 				
 		cd syn/gsi_exploder5/exploder5_csco_tr
 		cp exploder5_csco_tr.jic exploder5_csco_tr.rpd exploder5_csco_tr.sof $WEB_SRVR_PATH/gateware
@@ -33,7 +35,8 @@ case $PWD in
 
 		make pexarria5-clean
                 export PATH=$PATH:$pex_path/toolchain/bin
-		make pexarria5
+		export GSI_BUILD_TYPE=Balloon_release
+		make pexarria5 2>&1 > $log_makefile/nightly_build_pexarria5.$(date +%Y-%m-%d).log
 
 		cd syn/gsi_pexarria5/control
 		cp  pci_control.jic pci_control.sof pci_control.rpd $WEB_SRVR_PATH/gateware
@@ -46,7 +49,8 @@ case $PWD in
 
 		make vetar2a-clean
                 export PATH=$PATH:$vet_path/toolchain/bin
-		make vetar2a
+		export GSI_BUILD_TYPE=Balloon_release
+		make vetar2a 2>&1 > $log_makefile/nightly_build_vetar2a.$(date +%Y-%m-%d).log
 
 		cd syn/gsi_vetar2a/wr_core_demo
 		cp vetar2a.jic vetar2a.rpd vetar2a.sof $WEB_SRVR_PATH/gateware
@@ -59,7 +63,8 @@ case $PWD in
 
 		make scu3-clean
                 export PATH=$PATH:$scu3_path/toolchain/bin
-		make scu3
+		export GSI_BUILD_TYPE=Balloon_release
+		make scu3 2>&1 > $log_makefile/nightly_build_scu3.$(date +%Y-%m-%d).log
 
 		cd syn/gsi_scu/control3
 		cp scu_control.rpd $WEB_SRVR_PATH/gateware/scu_control3.rpd
@@ -74,7 +79,8 @@ case $PWD in
 
                 make scu2-clean
 		export PATH=$PATH:$scu2_path/toolchain/bin
-                make scu2
+		export GSI_BUILD_TYPE=Balloon_release
+                make scu2 2>&1 > $log_makefile/nightly_build_scu2.$(date +%Y-%m-%d).log
 
                 cd syn/gsi_scu/control2
                 cp scu_control.rpd $WEB_SRVR_PATH/gateware/scu_control2.rpd
@@ -89,11 +95,14 @@ case $PWD in
 
                 make firmware
 		cd syn/gsi_pexarria5/ftm
+		make clean
                 export PATH=$PATH:$dm_path/toolchain/bin
-		make
+		export GSI_BUILD_TYPE=Balloon_release
+		make 2>&1 > $log_makefile/nightly_build_ftm.$(date +%Y-%m-%d).log
 		
 		cp ftm.jic ftm.rpd ftm.sof $WEB_SRVR_PATH/gateware
-                
+                cp ftm.bin $WEB_SRVR_PATH/ftm_firmware
+ 
                 cd $WEB_SRVR_PATH/gateware
                 md5sum *.jic *.rpd *.sof | tee MD5SUMS
         ;;
@@ -103,14 +112,16 @@ case $PWD in
                 make 
                 cd ip_cores/wrpc-sw        
                 export PATH=$PATH:$wrpc_path/toolchain/bin
-                make
+		export GSI_BUILD_TYPE=Balloon_release
+                make 2>&1 > $log_makefile/nightly_build_wrpc-sw.$(date +%Y-%m-%d).log
 		
 		cp wrc.bin wrc.elf wrc.mif $WEB_SRVR_PATH/wrpc-sw
         ;;
 
 	$eb_path)
 
-                make etherbone
+		export GSI_BUILD_TYPE=Balloon_release
+                make etherbone 2>&1 > $log_makefile/nightly_build_etherbone.$(date +%Y-%m-%d).log
                 export PATH=$PATH:$eb_path/toolchain/bin
                 make etherbone-install STAGING=/tmp/etherbone
                 cd /tmp
@@ -122,7 +133,8 @@ case $PWD in
 
         $saftlib_path)
 
-                make saftlib
+		export GSI_BUILD_TYPE=Balloon_release
+                make saftlib 2>&1 > $log_makefile/nightly_build_saftlib.$(date +%Y-%m-%d).log
                 export PATH=$PATH:$saftlib_path/toolchain/bin
                 make saftlib-install STAGING=/tmp/saftlib
 		cd /tmp
