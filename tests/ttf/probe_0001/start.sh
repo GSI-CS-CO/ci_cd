ab26faa491f8700130d65531e6aaaaa30238d032
root <root@fatman2.acc.gsi.de>
2017-04-21 14:13:44 +0200

    probe_0001: added multiple vetar support

--- tests/ttf/probe_0001/start.sh
+++ tests/ttf/probe_0001/start.sh
@@ -56,6 +56,60 @@ function control_saftd()
     fi
     pex_id=$((pex_id+1))
   done
+
+  # Vetar
+  vetar_id=0  
+  saftd_args=""
+  saftd_devs=0
+  saftd_host=""
+  saftd_host_prev=""
+  for i in ${ttf_vetar_names[@]}; do
+    if [ $1 -eq 0 ]; then
+      echo "Stopping saftd... ($i@${ttf_vetar_hosts[$vetar_id]})"
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "killall saftd; pkill -f saft" > /dev/null 2>&1
+    elif [ $1 -eq 1 ]; then
+      echo "Starting saftd... ($i@${ttf_vetar_hosts[$vetar_id]})"
+      # While host name is still the same -> collect devices
+      if [ $saftd_devs -ne 0 ]; then
+        saftd_host_prev=$saftd_host
+      else
+        saftd_host_prev=${ttf_vetar_hosts[$vetar_id]}
+      fi
+      saftd_host=${ttf_vetar_hosts[$vetar_id]}
+      if [ "$saftd_host" == "$saftd_host_prev" ]; then
+        # Collect arguments for saftd
+        saftd_args=$saftd_args$i:${ttf_vetar_dev_ids[$saftd_devs]}" "
+        saftd_devs=$((saftd_devs+1))
+        if [ $saftd_devs -eq ${#ttf_vetar_names[@]} ]; then
+          # Given list ends here
+          ssh $ttf_vetar_user@$saftd_host_prev.$tff_postfix "/usr/sbin/saftd $saftd_args" > /dev/null 2>&1
+          ssh $ttf_vetar_user@$saftd_host_prev.$tff_postfix "ps -ax | grep saftd | grep -v grep" 2>/dev/null 
+        fi
+      elif [ "$saftd_host" != "$saftd_host_prev" ]; then
+        # Found other host
+        ssh $ttf_vetar_user@$saftd_host_prev.$tff_postfix "saftd $saftd_args" > /dev/null 2>&1
+        ssh $ttf_vetar_user@$saftd_host_prev.$tff_postfix "ps -ax | grep saftd | grep -v grep" 2>/dev/null 
+        saftd_args=""
+        saftd_devs=0
+      else
+        echo "Something is wrong with the configuration file!"
+      fi
+    else
+      echo "======================================================================"
+      echo "Probing... ($i@${ttf_vetar_hosts[$vetar_id]}@$ttf_vetar_dev_id)"
+      echo $ttf_vetar_user ${ttf_vetar_hosts[$vetar_id]} $tff_postfix  $i 
+      sleep 5
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-ctl $i -i"; echo ""
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-ctl $i -s"; echo ""
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-io-ctl $i -i"; echo ""
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-io-ctl $i -l"; echo ""
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "eb-info ${ttf_vetar_dev_ids[$vetar_id]}"; echo ""
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "eb-ls ${ttf_vetar_dev_ids[$vetar_id]}"; echo ""
+      echo "======================================================================"
+    fi
+    vetar_id=$((vetar_id+1))
+  done
+
   # SCUs
   scu_id=0
   for i in ${ttf_scu_names[@]}; do
@@ -78,29 +132,6 @@ function control_saftd()
       echo "======================================================================"
     fi
     scu_id=$((scu_id+1))
-  done
-  # Vetars
-  vetar_id=0
-  for i in ${ttf_vetar_names[@]}; do
-    if [ $1 -eq 0 ]; then
-      echo "Stopping saftd... ($i@${ttf_vetar_hosts[$vetar_id]})"
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "killall saftd; pkill -f saft" > /dev/null 2>&1
-    elif [ $1 -eq 1 ]; then
-      echo "Starting saftd... ($i@${ttf_vetar_hosts[$vetar_id]})"
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "/usr/sbin/saftd $ttf_default_saft_dev:$ttf_vetar_dev_id"
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "ps | grep saftd | grep -v grep" 2>/dev/null 
-    else
-      echo "======================================================================"
-      echo "Probing... ($i@${ttf_vetar_hosts[$vetar_id]}@$ttf_vetar_dev_id)"
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-ctl $ttf_default_saft_dev -i"; echo ""
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-ctl $ttf_default_saft_dev -s"; echo ""
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-io-ctl $ttf_default_saft_dev -i"; echo ""
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-io-ctl $ttf_default_saft_dev -l"; echo ""
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "eb-info $ttf_vetar_dev_id"; echo ""
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "eb-ls $ttf_vetar_dev_id"; echo ""
-      echo "======================================================================"
-    fi
-    vetar_id=$((vetar_id+1))
   done
   # exploders
   exploder_id=0
@@ -164,7 +195,37 @@ function config_leds()
                                                                         saft-io-ctl $i -n LED4_BASE_W -c 0x0 0x0 250000000 0xf 0 -u;"
     fi
     pex_id=$((pex_id+1))
+  done  
+  
+  # Vetar
+  vetar_id=0
+  for i in ${ttf_vetar_names[@]}; do
+    if [ $1 -eq 0 ]; then
+      echo "Disabling LEDs... ($i@${ttf_vetar_hosts[$vetar_id]})"
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix  "saft-io-ctl $i -n LED9 -x;\
+                                                                      saft-io-ctl $i -n LED10 -x;\
+                                                                      saft-io-ctl $i -n LED11 -x;\
+                                                                      saft-io-ctl $i -n LED12 -x;\
+                                                                      saft-io-ctl $i -n LED_DACK -x;"
+
+
+    else
+      echo "Enabling LEDs... ($i@${ttf_vetar_hosts[$vetar_id]})"
+      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-io-ctl $i -n LED9 -c 0x0 0x0 0 0xf 1 -u;\
+                                                                      saft-io-ctl $i -n LED9 -c 0x0 0x0 31250000 0xf 0 -u;\
+                                                                      saft-io-ctl $i -n LED10 -c 0x0 0x0 0 0xf 1 -u;\
+                                                                      saft-io-ctl $i -n LED10 -c 0x0 0x0 62500000 0xf 0 -u;\
+                                                                      saft-io-ctl $i -n LED11 -c 0x0 0x0 0 0xf 1 -u;\
+                                                                      saft-io-ctl $i -n LED11 -c 0x0 0x0 125000000 0xf 0 -u;\
+                                                                      saft-io-ctl $i -n LED12 -c 0x0 0x0 0 0xf 1 -u;\
+                                                                      saft-io-ctl $i -n LED12 -c 0x0 0x0 250000000 0xf 0 -u;\
+                                                                      saft-io-ctl $i -n LED_DACK -c 0x0 0x0 0 0xf 1 -u;\
+                                                                      saft-io-ctl $i -n LED_DACK -c 0x0 0x0 31250000 0xf 0 -u;"
+
+    fi
+    vetar_id=$((vetar_id+1))
   done
+
   # SCUs
   scu_id=0
   for i in ${ttf_scu_names[@]}; do
@@ -175,31 +236,7 @@ function config_leds()
     fi
     scu_id=$((scu_id+1))
   done
-  # Vetars
-  vetar_id=0
-  for i in ${ttf_vetar_names[@]}; do
-    if [ $1 -eq 0 ]; then
-      echo "Disabling LEDs... ($i@${ttf_vetar_hosts[$vetar_id]})"
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-io-ctl baseboard -n LED9 -x;\
-                                                                      saft-io-ctl baseboard -n LED10 -x;\
-                                                                      saft-io-ctl baseboard -n LED11 -x;\
-                                                                      saft-io-ctl baseboard -n LED12 -x;\
-                                                                      saft-io-ctl baseboard -n LED_DACK -x;"
-    else
-      echo "Enabling LEDs... ($i@${ttf_vetar_hosts[$vetar_id]})"
-      ssh $ttf_vetar_user@${ttf_vetar_hosts[$vetar_id]}.$tff_postfix "saft-io-ctl baseboard -n LED9 -c 0x0 0x0 0 0xf 1 -u;\
-                                                                      saft-io-ctl baseboard -n LED9 -c 0x0 0x0 31250000 0xf 0 -u;\
-                                                                      saft-io-ctl baseboard -n LED10 -c 0x0 0x0 0 0xf 1 -u;\
-                                                                      saft-io-ctl baseboard -n LED10 -c 0x0 0x0 62500000 0xf 0 -u;\
-                                                                      saft-io-ctl baseboard -n LED11 -c 0x0 0x0 0 0xf 1 -u;\
-                                                                      saft-io-ctl baseboard -n LED11 -c 0x0 0x0 125000000 0xf 0 -u;\
-                                                                      saft-io-ctl baseboard -n LED12 -c 0x0 0x0 0 0xf 1 -u;\
-                                                                      saft-io-ctl baseboard -n LED12 -c 0x0 0x0 250000000 0xf 0 -u;\
-                                                                      saft-io-ctl baseboard -n LED_DACK -c 0x0 0x0 0 0xf 1 -u;\
-                                                                      saft-io-ctl baseboard -n LED_DACK -c 0x0 0x0 31250000 0xf 0 -u;"
-    fi
-    vetar_id=$((vetar_id+1))
-  done
+  
   # exploders
   exploder_id=0
   for i in ${ttf_exploder_names[@]}; do
