07a5122891f636e7966e9e7fa7471d84ae5dfd9a
A. Hahn <a.hahn@gsi.de>
2017-08-02 16:23:41 +0200

    build_target: added build type

--- scripts/nightly/build_target.sh
+++ scripts/nightly/build_target.sh
@@ -12,12 +12,13 @@ v_webserver_base=/var/www/html/releases/;
 v_build_target=$1;
 v_target_branch=$2;
 v_webserver_target=$3;
+v_build_type=$4
 
 # =============================================================================
 # Check arguments
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
   echo "Sorry we need at least 3 parameters..."
-  echo "Example: ./build_target.sh <target> <branch> <webserver target>"
+  echo "Example: ./build_target.sh <target> <branch> <webserver target> <build type>"
   exit 1
 fi
 
@@ -41,6 +42,7 @@ git checkout $v_target_branch
 # Start build
 ./fix-git.sh
 ./install-hdlmake.sh
+export GSI_BUILD_TYPE=$v_build_type
 make $v_build_target
 
 # =============================================================================
