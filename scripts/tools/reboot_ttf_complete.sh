01579d5f4d678b1bd5c34eab276edc1c48314bb4
A. Hahn <a.hahn@gsi.de>
2017-10-11 14:39:41 +0200

    reboot_ttf -> updated switches

--- scripts/tools/reboot_ttf_complete.sh
+++ scripts/tools/reboot_ttf_complete.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Topology -> Top Down
-ssh root@192.168.20.40 '/sbin/reboot' # Betriebsgebaeude - TTF Clock Master
-ssh root@192.168.20.45 '/sbin/reboot' # Betriebsgebaeude - TTF Data Master Connection
-ssh root@192.168.20.20 '/sbin/reboot' # SE Messtation - TTF Switch 1
-ssh root@192.168.20.18 '/sbin/reboot' # SE Messtation - TTF Switch 2
-ssh root@192.168.20.12 '/sbin/reboot' # SE Messtation - TTF Switch 3
-ssh root@192.168.20.11 '/sbin/reboot' # SE Messtation - TTF Switch 4
+ssh root@192.168.20.50 '/sbin/reboot' # nwt0037m66 - Betriebsgebaeude - TTF Clock Master
+ssh root@192.168.20.55 '/sbin/reboot' # nwt0042m66 - Betriebsgebaeude - TTF Data Master and Management
+ssh root@192.168.20.21 '/sbin/reboot' # nwt0009m66 - Vorortlabor - TTF Switch #1
+ssh root@192.168.20.22 '/sbin/reboot' # nwt0010m66 - Vorortlabor - TTF Switch #2
+ssh root@192.168.20.28 '/sbin/reboot' # nwt0016m66 - Vorortlabor - TTF Switch #3
+ssh root@192.168.20.30 '/sbin/reboot' # nwt0018m66 - Vorortlabor - TTF Switch #4
+ssh root@192.168.20.13 '/sbin/reboot' # nwt0011m66 - Vorortlabor - TTF Switch #5
