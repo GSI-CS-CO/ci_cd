4758f5454c6a1583234ae68eff19bd6c2310e172
A. Hahn <a.hahn@gsi.de>
2019-03-08 09:51:57 +0100

    git_init.sh: initial

---
+++ scripts/aco_builder/git_init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# =========================================================
+# GIT Settings (Don't Edit This Area!)
+git config --global user.name "Timing Group Jenkins"
+git config --global user.email "csco-tg@gsi.de"
