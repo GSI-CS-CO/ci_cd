9353865afb2c1490452c9e2f3e051bf3779277f0
A. Hahn <a.hahn@gsi.de>
2019-03-12 16:35:37 +0100

    build_gateware.sh: added gsi build type

--- scripts/aco_builder/build_gateware.sh
+++ scripts/aco_builder/build_gateware.sh
@@ -5,6 +5,7 @@
 export CFG_QUARTUS_VERSION=16 # 16 or 18
 export CFG_BRANCH=doomsday # doomsday, master, ...
 export CFG_TARGET=scu2 # scu2, scu3, vetar2a, vetar2a-ee-butis, exploder5, pexarria5, microtca, pmc
+export GSI_BUILD_TYPE="Doomsday-v4.X.Y" # release name
 
 # =========================================================
 # Environmental Settings (Don't Edit This Area!)
