a94cd74b5e82eedb013d794c2800dd3061ae2035
A. Hahn <a.hahn@gsi.de>
2019-07-10 11:03:29 +0200

    pps_0002: scope -> changed NavigationToolbar2QTxy import

--- tests/ttf/pps_0002/scope.py
+++ tests/ttf/pps_0002/scope.py
@@ -15,7 +15,8 @@ import numpy as np
 from PyQt4 import QtGui
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+#from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 
 ########################################################################################################################
 l_sorted_edges = []
