8de25a5ad7956aa06047120c3f6e0a539e6cd4b9
A. Hahn <a.hahn@gsi.de>
2019-07-12 17:14:12 +0200

    ttf: service_check_tr_state.sh initial

---
+++ tests/ttf/tools/icinga2/service_check_tr_state.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/common/usr/timing/lib
+export PATH=$PATH:/common/usr/timing/bin
+
+# Usage: ./script 10.10.10.1
+trackphase="TRACKING"
+while getopts H: option
+do
+ case "${option}"
+ in
+H) addr=${OPTARG};;
+ esac
+done
+
+# Check White Rabbit Status
+state=$(eb-mon udp/$addr -s)
+ret_val=$?
+if [ $ret_val -ne 0 ]; then
+    echo "White Rabbit state: Unknown"
+    exit 2
+fi
+
+# Check for current release
+res=$(echo $state | grep $trackphase)
+ret_val=$?
+if [ $ret_val -ne 0 ]; then
+    echo "White Rabbit state: $res"
+    exit 1
+else
+    echo "White Rabbit state: $res"
+    exit 0
+fi
