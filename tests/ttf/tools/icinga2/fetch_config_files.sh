32d043058944bf69866c76dbe77a2afd6da12c5e
A. Hahn <a.hahn@gsi.de>
2019-07-12 15:31:39 +0200

    ttf: added fetch_config_files.sh

---
+++ tests/ttf/tools/icinga2/fetch_config_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set +e
+v_repo=https://raw.githubusercontent.com/GSI-CS-CO/ci_cd/master/tests/ttf/tools/icinga2
+for v_file in groups.conf hosts.conf services.conf commands.conf service_check_tr_gateware.sh fetch_config_files.sh
+do
+  rm $v_file
+  wget $v_repo/$v_file
+done
+sudo systemctl reload icinga2
