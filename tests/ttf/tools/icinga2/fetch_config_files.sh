2acae9cf2127f1a02e6b28233e0a2ca653936add
A. Hahn <a.hahn@gsi.de>
2019-07-12 17:15:38 +0200

    ttf: updated commands.conf and services.conf

--- tests/ttf/tools/icinga2/fetch_config_files.sh
+++ tests/ttf/tools/icinga2/fetch_config_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set +e
 v_repo=https://raw.githubusercontent.com/GSI-CS-CO/ci_cd/master/tests/ttf/tools/icinga2
-for v_file in groups.conf hosts.conf services.conf commands.conf service_check_tr_gateware.sh fetch_config_files.sh
+for v_file in groups.conf hosts.conf services.conf commands.conf service_check_tr_gateware.sh service_check_tr_state.sh service_check_tr_acquired_locks.sh fetch_config_files.sh
 do
   rm $v_file
   wget $v_repo/$v_file
