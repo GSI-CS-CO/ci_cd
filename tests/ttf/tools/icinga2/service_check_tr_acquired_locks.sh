c221ef9066403688f36550bf726d3fca7368a72c
A. Hahn <a.hahn@gsi.de>
2019-07-12 17:12:36 +0200

    ttf: service_check_tr_acquired_locks.sh initial

---
+++ tests/ttf/tools/icinga2/service_check_tr_acquired_locks.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/common/usr/timing/lib
+export PATH=$PATH:/common/usr/timing/bin
+
+# Usage: ./script 10.10.10.1
+while getopts H: option
+do
+ case "${option}"
+ in
+H) addr=${OPTARG};;
+ esac
+done
+
+# Get acquired locks
+locks=$(eb-mon udp/$addr -g | grep locks | awk '{print $5}')
+ret_val=$?
+if [ $ret_val -ne 0 ]; then
+    echo "Acquired locks unknown"
+    exit 2
+fi
+
+# Process number of acquired locks
+if [ $locks -eq 0 ]; then
+    echo "Lock never acquired"
+    exit 2
+elif [ $locks -gt 2 ]; then
+    echo "Lock(s) acquired: $locks"
+    exit 1
+else
+    echo "Lock(s) acquired: $locks"
+    exit 0
+fi
