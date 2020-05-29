eadfed480944643fa0d7c2dd1ab21630b688f87b
A. Hahn <a.hahn@gsi.de>
2020-05-29 17:28:37 +0200

    icmp_0002: added random and host feature, fixed remote bugs

--- tests/ttf/icmp_0002/generator.py
+++ tests/ttf/icmp_0002/generator.py
@@ -6,12 +6,34 @@ import subprocess
 import sys
 import json
 import time
+import getopt
+import random
+
+########################################################################################################################
+v_fixed_min_count = 1 # Don't edit this
+v_fixed_min_interval = 0.2 # Don't edit this
+
+########################################################################################################################
+v_min_interval = v_fixed_min_interval
+v_max_count = v_fixed_min_count
+v_max_size = 56
+v_para_count = 1
+v_random_mode = 0
+v_host_mode = 0
+v_verbose = 0
+
+########################################################################################################################
+def func_print_help():
+    print("TBD -> Help")
 
 ########################################################################################################################
 def func_generate_test_configuration():
     devices=0
-    interval=0.5
-    count=1000
+    random_count=v_fixed_min_count
+    random_interval=v_fixed_min_interval
+    random.seed(1)
+
+    # Parse devices file
     try:
         with open('../devices.json') as json_file:
             data = json.load(json_file)
@@ -21,20 +43,41 @@ def func_generate_test_configuration():
             f.write("declare -a devices=(")
             for p in data:
                 for q in p['receivers']:
-                    f.write("%s " % q['iptg'])
-                    devices=devices+1
+                    for i in range(v_para_count):
+                        if v_host_mode == 0:
+                            f.write("%s " % q['iptg'])
+                        else:
+                            f.write("%s%s " % (p['name'], p['extension']))
+                        devices=devices+1
             f.write(")\n")
 
             # Write individual interval
             f.write("declare -a interval=(")
             for x in range(devices):
-                f.write("%s " % interval)
+                for i in range(v_para_count):
+                    if v_random_mode == 0:
+                        f.write("%s " % v_min_interval)
+                    else:
+                        random_interval = random.uniform(v_fixed_min_interval, float(v_min_interval))
+                        f.write("%s " % str(random_interval))
             f.write(")\n")
 
             # Write individual count
             f.write("declare -a count=(")
             for x in range(devices):
-                f.write("%s " % count)
+                for i in range(v_para_count):
+                    if v_random_mode == 0:
+                        f.write("%s " % v_max_count)
+                    else:
+                        random_count = random.randint(v_fixed_min_count, int(v_max_count))
+                        f.write("%s " % str(random_count))
+            f.write(")\n")
+
+            # Write individual count
+            f.write("declare -a size=(")
+            for x in range(devices):
+                for i in range(v_para_count):
+                    f.write("%s " % v_max_size)
             f.write(")\n")
 
             f.close()
@@ -43,7 +86,55 @@ def func_generate_test_configuration():
 
 ########################################################################################################################
 def main():
-    func_generate_test_configuration()
+    global v_min_interval
+    global v_max_count
+    global v_max_size
+    global v_para_count
+    global v_random_mode
+    global v_host_mode
+    global v_verbose
+    v_print_help = 0
+
+    # Get arguments
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "c:i:s:p:xthv", ["v_max_count=", "v_min_interval=", "v_max_size=", "v_para_count=", "v_random_mode", "v_host_mode", "help", "verbose"])
+    except getopt.GetoptError, err:
+        print(err)
+        sys.exit(1)
+
+    for opt, args in opts:
+        if opt in ('-c'):
+            v_max_count = args
+        elif opt in ('-i'):
+            v_min_interval = args
+        elif opt in ('-s'):
+            v_max_size = arg
+        elif opt in ('-p'):
+            v_para_count = int(args)
+        elif opt in ('-x'):
+            v_random_mode = 1
+        elif opt in ('-t'):
+            v_host_mode = 1
+        elif opt in ('-v'):
+            v_verbose = 1
+        else:
+            v_print_help = 1
+
+    # Print help (if wanted) or ...
+    if v_print_help != 0:
+        func_print_help()
+    # ... generate test test configuration
+    else:
+        print("Generating test configuration...")
+        if v_verbose != 0:
+            print("Parameters:")
+            print("-- Packet count (max): %s" % str(v_max_count))
+            print("-- Interval (min): %s" % str(v_min_interval))
+            print("-- Packet size (max): %s" % str(v_max_size))
+            print("-- Parallel processes: %s" % str(v_para_count))
+            print("-- Random mode: %s" % str(v_random_mode))
+            print("-- Scan hosts: %s" % str(v_host_mode))
+        func_generate_test_configuration()
 
     # Done
     exit(0)
--- tests/ttf/icmp_0002/tester.sh
+++ tests/ttf/icmp_0002/tester.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
+
 # Local settings
-log_folger=log
+log_folder=icmp_test_log
 log_extension=log
+verbose=0
 
 # Load test settings
 source $(find . -name "test_configuration.sh")
 
 # clean up
-rm -f *.$log_extension
+rm -f $(pwd)/$log_folder/*.$log_extension
+mkdir $(pwd)/$log_folder
+
 pids=()
 test_failed=0
 cnt=0
+sub_id=0
+sub_id_text="0"
 
 # start processes
 for i in "${devices[@]}"
 do
-   ping -c ${count[${cnt}]} -i ${interval[${cnt}]} "$i" | tee -a $i.$log_extension &
+   ping -c ${count[${cnt}]} -i ${interval[${cnt}]} -s ${size[${cnt}]} "$i" | tee -a "$(pwd)/"$log_folder/$i"_$sub_id_text.$log_extension" &
    pids+=("$!")
    cnt=$cnt+1
+   sub_id=$sub_id+1
+   sub_id_text="$((sub_id))"
 done
 
 # wait until every command was executed
@@ -26,29 +34,42 @@ do
   echo "Waiting for PID $pid..."
   wait $pid
 done
+echo "Found $sub_id_text subprocess..."
 
 # analyze log files
+echo ""
 echo "*******************************************************************************"
 echo "*********************************** Results ***********************************"
 echo "*******************************************************************************"
 echo ""
 
+sub_id=0
+sub_id_text="0"
 for i in "${devices[@]}"
 do
   echo "$i statistics:"
-  cat $i.$log_extension | grep "0% packet loss"
+  cat "$(pwd)/"$log_folder/$i"_$sub_id_text.$log_extension" | grep " 0% packet loss"
   retVal=$?
   if [ $retVal -ne 0 ]; then
-    echo "Error"
+    echo "Error!"
+    cat "$(pwd)/"$log_folder/$i"_$sub_id_text.$log_extension" | grep "packet loss"
     test_failed=1
   fi
+  sub_id=$sub_id+1
+  sub_id_text="$((sub_id))"
 done
 
 # store log files in case of failure
 if [ $test_failed -ne 0 ]; then
+  echo "Error: Test failed for one or more device(s)!"
   folder=$(date +%Y%m%d_%H%M%S)
-  mkdir $log_folger/$folder
-  cp *.$log_extension $log_folger/$folder
+  cd $(pwd)/$log_folder
+  mkdir $(pwd)/$folder
+  cd ..
+  cp $(pwd)/$log_folder/*.$log_extension $(pwd)/$log_folder/$folder
+else
+  echo ""
+  echo "Info: Test succeeded for all devices!"
 fi
 
 # finish and return test status
