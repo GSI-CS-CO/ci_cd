8a336a84bd4344a708b30bb7cf393a7c75b495ce
alex-cscotg <alex-cscotg@users.noreply.github.com>
2021-06-21 09:20:17 +0200

    ttf: wrs_simple_update -> added "reboot only" feature

--- tests/ttf/tools/wrs_simple_update/update-all.sh
+++ tests/ttf/tools/wrs_simple_update/update-all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 list=update.list
 firmware=$1
+reboot_only=0
 
 # Check if at least one argument is given
 if [ $# -ne 1 ]; then
-  echo "Error: Illegal number of parameters, please provide a valid firmware file!"
+  echo "Error: Illegal number of parameters, please provide a valid firmware file (or reboot)!"
   exit 1
 fi
 
@@ -13,8 +14,13 @@ if [ -f $firmware ]; then
   echo "Info: Using $firmware to update a/all TTF switch(es)!"
   md5sum $firmware
 else
-  echo "Error: Sorry, can't find file $firmware!"
-  exit 1
+  if [ $firmware == "reboot" ]; then
+    reboot_only=1
+    echo "Info: Rebooting switch(es)!"
+  else
+    echo "Error: Sorry, can't find file $firmware!"
+    exit 1
+  fi
 fi
 
 # Remove old list (if file exists)
@@ -37,8 +43,10 @@ for line in $lines; do
   echo ""
   echo "**************************************************"
   echo "Info: Updating $line now..."
-  echo "-> Deploying firmware..."
-  scp $firmware $line":/update"
+  if [ $reboot_only -eq 0 ]; then
+    echo "-> Deploying firmware..."
+    scp $firmware $line":/update"
+  fi
   echo "-> Rebooting switch..."
   ssh $line reboot
 done
